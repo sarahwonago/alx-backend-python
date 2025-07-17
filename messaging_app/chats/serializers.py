97608cd1138df61bc50658551b8429eab3cbf9d4
sarahwonago <sarahshaaleh@gmail.com>
2025-07-17 13:10:56 +0300

    update serializers

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -3,6 +3,9 @@ from .models import User, Conversation, Message
 
 
 class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+    phone_number = serializers.CharField(allow_blank=True, required=False)
+
     class Meta:
         model = User
         fields = [
@@ -10,14 +13,19 @@ class UserSerializer(serializers.ModelSerializer):
             'username',
             'first_name',
             'last_name',
+            'full_name',
             'email',
             'phone_number',
             'bio'
         ]
 
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
+
 
 class MessageSerializer(serializers.ModelSerializer):
     sender = UserSerializer(read_only=True)
+    message_body = serializers.CharField()
 
     class Meta:
         model = Message
@@ -33,6 +41,7 @@ class MessageSerializer(serializers.ModelSerializer):
 class ConversationSerializer(serializers.ModelSerializer):
     participants = UserSerializer(many=True, read_only=True)
     messages = MessageSerializer(many=True, read_only=True)
+    participant_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Conversation
@@ -40,5 +49,14 @@ class ConversationSerializer(serializers.ModelSerializer):
             'conversation_id',
             'participants',
             'created_at',
-            'messages'
+            'messages',
+            'participant_count'
         ]
+
+    def get_participant_count(self, obj):
+        return obj.participants.count()
+
+    def validate(self, data):
+        if not data.get('participants') and self.instance is None:
+            raise serializers.ValidationError("At least one participant is required to create a conversation.")
+        return data
