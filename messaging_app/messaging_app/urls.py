d8c270f79e222273379bad61bb7980e188588a57
sarahwonago <sarahshaaleh@gmail.com>
2025-07-17 14:16:30 +0300

    auth in browsable api

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('chats.urls')),
+    path('api/', include('chats.urls')),                  
+    path('api-auth/', include('rest_framework.urls')),    # <-- Enables login/logout in DRF browsable API
 ]
