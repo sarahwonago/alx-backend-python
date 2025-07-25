e092a8bf7be796c1645a89fe1658085af7d14d27
sarahwonago <sarahshaaleh@gmail.com>
2025-07-25 10:43:42 +0300

    Logging User Requests(Basic Middleware)

---
+++ Django-Middleware-0x03/chats/middleware.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+
+class RequestLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        user = request.user if request.user.is_authenticated else "Anonymous"
+        log_entry = f"{datetime.now()} - User: {user} - Path: {request.path}\n"
+
+        with open("requests.log", "a") as logfile:
+            logfile.write(log_entry)
+
+        response = self.get_response(request)
+        return response
