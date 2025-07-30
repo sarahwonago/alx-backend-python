92e32e9eac01546da15c4ad10042976acbc712a5
sarahwonago <sarahshaaleh@gmail.com>
2025-07-30 10:32:24 +0300

    Implement Signals for User Notifications

--- Django-signals_orm-0x04/messaging/apps.py
+++ Django-signals_orm-0x04/messaging/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class MessagingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'messaging'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "messaging"
+
+    def ready(self):
+        import messaging.signals
--- Django-signals_orm-0x04/messaging/tests.py
+++ Django-signals_orm-0x04/messaging/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Message, Notification
 
-# Create your tests here.
+
+class SignalTestCase(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create_user(username="alice", password="pass123")
+        self.user2 = User.objects.create_user(username="bob", password="pass456")
+
+    def test_notification_created_on_message(self):
+        message = Message.objects.create(
+            sender=self.user1, receiver=self.user2, content="Hi Bob!"
+        )
+        self.assertEqual(Notification.objects.count(), 1)
+        notification = Notification.objects.first()
+        self.assertEqual(notification.user, self.user2)
+        self.assertEqual(notification.message, message)
